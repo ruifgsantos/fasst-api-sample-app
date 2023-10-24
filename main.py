ba7d6b59b14c10654a02cc41ea2ec3515665834f
Rui Santos <rfgsantos>
2023-10-24 11:41:08 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home_page(request: Request):
+    return templates.TemplateResponse("template.html", {"request": request})
+
+
+@app.get("/{name}", response_class=HTMLResponse)
+async def home_page(request: Request, name: str):
+    return templates.TemplateResponse("template2.html", {"request": request, "name": name})
+
+
+@app.get("/home", response_class=HTMLResponse)
+async def message():
+    return {"message": "Hello World"}
